bbd7bb3295318ea651a4a0dfeb787286587c8137
Aniruddhsinh Jadeja <Ajadeja3102@conestogac.on.ca>
2023-03-11 22:30:27 -0500

    Adds a subject view to the layout
    This commit modifies layout_activity_main, adds a subject view, reposition the hello msg and logout button, repositions recyclerview, adds a fade disappearance to the recyclerview on top, adds a temporary bottom backdrop.
    
    Modifies item_view_layout.xml and reduces the size of card and repositions the elements.
    New Activities :
    
    New Layouts :
    New Files :
    backdrop.png
    backdrop_top.png
    rounded_rect.xml
    
    New Features :
    
    Changes in files :
         layout_main_activity :
         + Adds a subject view
         M Repositions recyclerview
    
         strings.xml
         + Adds subject strings
    
         colors.xml
         + Adds an light orange color
    
         item_view_layout.xml
         M reduces the size and repositions the elements
    
         MainActivity.java
         + Adds a code to scroll nestedscrollview to 0
    
    Known Bugs : Assignment text is not in optimal position
    Repro steps :

--- app/src/main/java/com/conestogac/mobileapplicationdevelopment/assignophobia/MainActivity.java
+++ app/src/main/java/com/conestogac/mobileapplicationdevelopment/assignophobia/MainActivity.java
@@ -1,6 +1,7 @@
 package com.conestogac.mobileapplicationdevelopment.assignophobia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,6 +11,7 @@ import android.view.View;
 
 import com.conestogac.mobileapplicationdevelopment.assignophobia.utils.posts.Item;
 import com.conestogac.mobileapplicationdevelopment.assignophobia.utils.posts.PostAdapter;
+import com.google.android.material.appbar.AppBarLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
     List<Item> items = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        NestedScrollView nestedScrollView = findViewById(R.id.nested_scroll_view);
         recyclerView = findViewById(R.id.posts_recycler_view);
 
         items.add(new Item(R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,"Hannah","Assignment"));
@@ -41,11 +44,10 @@ public class MainActivity extends AppCompatActivity {
         items.add(new Item(R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,"Hannah","Assignment"));
         items.add(new Item(R.drawable.ic_launcher_background,R.drawable.ic_launcher_background,"Hannah","Assignment"));
 
-
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new PostAdapter(getApplicationContext(),items));
 
-
+        nestedScrollView.post(() -> nestedScrollView.scrollTo(0, 0));
 
     }
 }
