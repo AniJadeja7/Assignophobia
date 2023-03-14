03b3b96d70ed1931199ade6ed24414871a046fc5
Aniruddhsinh Jadeja <Ajadeja3102@conestogac.on.ca>
2023-03-14 18:31:53 -0400

    Adds a profile activity
    This commit adds a profile activity with a layout file
    This commit creates a default layout of the activity without java file.
    
    New Activities :
    ProfileActivity.java
    New Layouts :
    activity_profile.xml
    New Files :
    New Features :
    
    Changes in files :
         strings.xml
         + adds profile string
         + adds whispered string
         + adds memed string
         + adds save string
         + adds student email string
         + adds student name string
         + adds college string
         + adds student number string
    
         colors.xml
         + adds a white_800 color
    
    Known Bugs :
    Repro steps :

---
+++ app/src/main/java/com/conestogac/mobileapplicationdevelopment/assignophobia/activities/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.conestogac.mobileapplicationdevelopment.assignophobia.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.conestogac.mobileapplicationdevelopment.assignophobia.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
