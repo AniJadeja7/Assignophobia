d9fb4c5d599e35c9be0f295b2e770afc22412361
Aniruddhsinh Jadeja <Ajadeja3102@conestogac.on.ca>
2023-03-26 16:05:39 -0400

    Adds The functionality of paraphrasing the text using OpenAI LLM and uploading the paraphrased version.
    This commit adds main code to the WhisperActivity.java and adds createPrompt() method to create the prompt, getAIRephrasedText() to rephrase the text and implements an interface for making system wait till the text is paraphrased.
    
    New Activities :
    New Layouts:
    New Files :
    New Features : Paraphrasing the Text
    
    Changes in files :
          WhisperActivity.java
          + Adds OkHttpClient and Request object to make requests and get response.
          + Adds createPrompt() method which has command and the userInput which makes up the prompt.
          + Adds a rephrasedAssignmentText variable inside createPublicPost
          + Adds getAIRephrasedText() method which creates a requestBody object and creates a new call which has two methods onFailure() and onResponse().
               - onResponse() gets the Json Object and fetches the text from it. onResponse() also stores the text inside generatedText[0].
    
          build.gradle(app)
          + Adds dependency for okhttp and gson
    
    Known Bugs :   Bug 1.0
                 + Bug 2.0
                       does not correctly validates the user input in the ProfileActivity.java
    Repro steps :
    
    Notes :
    
    1. Needs to be tested what happens if the user is newly created and selects nothing and presses save button.
    2. Not sure if profile picture is getting updated everytime user selects the picture.
    3. Needs to present the user feedback on successful profile and data update.

--- app/src/main/java/com/conestogac/mobileapplicationdevelopment/assignophobia/activities/WhisperActivity.java
+++ app/src/main/java/com/conestogac/mobileapplicationdevelopment/assignophobia/activities/WhisperActivity.java
@@ -1,43 +1,24 @@
 package com.conestogac.mobileapplicationdevelopment.assignophobia.activities;
 
+import android.app.DatePickerDialog;
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.drawable.RoundedBitmapDrawable;
-import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory;
-
-import android.app.DatePickerDialog;
-import android.content.ContentResolver;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.LayerDrawable;
-import android.net.Uri;
-import android.os.Build;
-import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.webkit.MimeTypeMap;
-import android.widget.DatePicker;
-import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.CustomTarget;
-import com.bumptech.glide.request.transition.Transition;
 import com.conestogac.mobileapplicationdevelopment.assignophobia.R;
-import com.google.android.gms.auth.api.signin.internal.Storage;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.material.button.MaterialButton;
-import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
@@ -51,24 +32,44 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.google.gson.JsonObject;
 import com.makeramen.roundedimageview.RoundedImageView;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.HashMap;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 
-public class WhisperActivity extends AppCompatActivity implements View.OnClickListener{
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class WhisperActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String API_KEY = "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";
+    //private static final String API_URL = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions";
+    // private static final String API_URL = "https://api.openai.com/v1/engines/gpt-3.5-turbo/chat";
+    //private static final String API_URL = "https://api.openai.com/v1/chat/completions";
+    private static final String API_URL = "https://api.openai.com/v1/completions";
+    private static final String TYPE = "application/json";
+    private static final String CONTENT_TYPE = "Content-Type";
+    private static final String AUTHORIZATION = "Authorization";
 
     private static final String DATE_FORMAT = "MM/dd/yyyy";
     private static final String DATE_SEPARATOR = "/";
     private boolean isDelete;
 
     private static final String TAG = "WhisperActivity";
+    private static final String TAG2 = "FlowWhisperActivity";
     private static String imageExtension;
     private TextInputEditText dateEditText;
     private TextInputLayout dateLayout;
@@ -86,6 +87,16 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
 
     private DatabaseReference databaseReference;
     private int whisperedNumber;
+
+    private static final OkHttpClient client = new OkHttpClient();
+    private static final MediaType mediaType = MediaType.parse(TYPE);
+    private static Request request;
+    private static RequestBody body;
+
+    private JSONObject requestBody;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -94,10 +105,9 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
         init();
 
 
-
     }
 
-    private void init(){
+    private void init() {
         dateLayout = findViewById(R.id.date_picker_layout);
         dateEditText = findViewById(R.id.date_picker_edit_text);
         assignmentImagePicker = findViewById(R.id.upload_button);
@@ -119,7 +129,7 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         databaseReference = database.getReference();
         publicPostsDatabaseReference = databaseReference.child("publicPosts");
-        DatabaseReference personalPostReference = databaseReference.child("users/" + Objects.requireNonNull(currentUser).getUid() + "/publicPosts");
+        //DatabaseReference personalPostReference = databaseReference.child("users/" + Objects.requireNonNull(currentUser).getUid() + "/publicPosts");
 
 
 
@@ -129,6 +139,8 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     protected void onStart() {
         super.onStart();
+
+
         // Add click listener to end icon to open date picker
         dateLayout.setEndIconOnClickListener(v -> {
             // Create a Calendar instance
@@ -143,7 +155,7 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
             DatePickerDialog datePickerDialog = new DatePickerDialog(WhisperActivity.this,
                     (view, year1, month1, dayOfMonth1) -> {
                         // Set the selected date on the EditText
-                        dateEditText.setText(String.format("%02d/%02d/%04d", month1 +1, dayOfMonth1, year1));
+                        dateEditText.setText(String.format("%02d/%02d/%04d", month1 + 1, dayOfMonth1, year1));
                     }, year, month, dayOfMonth);
             datePickerDialog.show();
         });
@@ -153,12 +165,11 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 try {
                     whisperedNumber = Integer.parseInt(Objects.requireNonNull(snapshot
-                            .child("users/"+currentUser.getUid()).child("WhisperedNumber")
+                            .child("users/" + currentUser.getUid()).child("WhisperedNumber")
                             .getValue()).toString());
-                    Log.d(TAG, "onDataChange: current whispered posts "+whisperedNumber);
-                  }catch (Exception e)
-                {
-                    Log.d(TAG, "onDataChange: "+e.getMessage());
+                    Log.d(TAG, "onDataChange: current whispered posts " + whisperedNumber);
+                } catch (Exception e) {
+                    Log.d(TAG, "onDataChange: " + e.getMessage());
                 }
 
             }
@@ -227,10 +238,29 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
 
     }
 
+    private String createPrompt() {
+        Log.d(TAG2, "createPrompt: called..");
+        String command = "Rephrase the following text : \n";
+        String userInput = Objects.requireNonNull(assignmentEditText.getText()).toString();
+       // String userInput = "\"" + "This is the final project of the Mobile Development Subject with subject code 8833102. " + "\"";
+        Log.d(TAG2, "createPrompt: prompt created "+ command+userInput+"*");
+       return command+userInput;
+    }
+
     private void createPublicPost() {
+
+        final String[] rephrasedAssignmentText = new String[1];
+        getAIRephrasedText(new AICompletionCallback() {
+            @Override
+            public void onCompletion(String generatedText) {
+                rephrasedAssignmentText[0] = generatedText;
+            }
+        });
+
+        Log.d(TAG2, "createPublicPost: uploading Rephrased Text : "+rephrasedAssignmentText[0]);
         UploadTask uploadTask = publicPostsStorageReference.child(
-                        Objects.requireNonNull(imageNameEditText.getText()) +"."
-                                +imageExtension)
+                        Objects.requireNonNull(imageNameEditText.getText()) + "."
+                                + imageExtension)
                 .putFile(assignmentImageReference);
         uploadTask.addOnSuccessListener(taskSnapshot -> {
 
@@ -240,15 +270,15 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
                 // Will create an UniqueId under "publicPosts" and will upload data under it.
 
                 DatabaseReference newPostRef = publicPostsDatabaseReference.push();
-                Map<String,String> postData = new HashMap<>();
-                postData.put("assignmentImageReference",uri.toString());
-                postData.put("assignmentText", Objects.requireNonNull(assignmentEditText.getText()).toString());
-                postData.put("displayUserName",currentUser.getDisplayName());
+                Map<String, String> postData = new HashMap<>();
+                postData.put("assignmentImageReference", uri.toString());
+                postData.put("assignmentText", rephrasedAssignmentText[0].trim());
+                postData.put("displayUserName", currentUser.getDisplayName());
                 postData.put("userProfileUrlReference", Objects.requireNonNull(currentUser.getPhotoUrl()).toString());
 
                 newPostRef.setValue(postData);
 
-                databaseReference.child("users/"+currentUser.getUid()).child("WhisperedNumber").setValue(whisperedNumber+1);
+                databaseReference.child("users/" + currentUser.getUid()).child("WhisperedNumber").setValue(whisperedNumber + 1);
 
 
             });
@@ -258,14 +288,93 @@ public class WhisperActivity extends AppCompatActivity implements View.OnClickLi
 
     }
 
-    private void createErrorSnackBar(String msg,boolean isError){
+    private void getAIRephrasedText(AICompletionCallback callback) {
+        final String[] generatedText = {""};
+
+        // Define the request body for the API CALL...
+        requestBody = new JSONObject();
+        try {
+            requestBody.put("model", "text-davinci-003");
+            requestBody.put("prompt",createPrompt());
+            requestBody.put("temperature", 0);
+            requestBody.put("max_tokens", 1500);
+            requestBody.put("top_p", 1.0);
+            requestBody.put("frequency_penalty", 0.2);
+            requestBody.put("presence_penalty", 0.0);
+            requestBody.put("stop", new JSONArray().put("*"));
+            Log.d(TAG2, "init: requestBody initialized");
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        // Define the request
+        Log.d(TAG2, "init: request object Initialized");
+        request = new Request.Builder()
+                .url(API_URL)
+                .addHeader(CONTENT_TYPE,TYPE)
+                .addHeader(AUTHORIZATION, "Bearer " + API_KEY)
+                .post(RequestBody.create(MediaType.parse(TYPE), requestBody.toString()))
+                .build();
+
+
+        try  {
+            //requestBody.put("prompt", "Rephrase the statement:\n\nThis is the final project of the Mobile Application Development with subject code 8833102.*");
+
+            Log.d(TAG2, "getAIRephrasedText: requestbody updated with promp");
+
+            client.newCall(request).enqueue(new Callback() {;
+                @Override
+                public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                    Log.d(TAG, "getAIRephrased Text => onFailure: "+e.getMessage());
+                }
+
+                @Override
+                public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                    String responseString = response.body().string();
+                    Log.d(TAG2, "getAIRephrasedText => onResponse: "+responseString);
+                    // Parse the response String as a JSONObject
+                    JSONObject responseObject = null;
+                    try {
+                        responseObject = new JSONObject(responseString);
+                    } catch (JSONException e) {
+                        Log.d(TAG, "onResponse: "+e.getMessage());
+                    }
+
+                    // Get the generated text from the response
+                    try {
+                        generatedText[0] = responseObject.getJSONArray("choices").getJSONObject(0).getString("text");
+                        generatedText[0] = generatedText[0].trim();
+                        Log.d(TAG, "getAIRephrasedText: Returning Generated text"+generatedText[0]);
+                        callback.onCompletion(generatedText[0]);
+                        Log.d(TAG2, "getAIRephrasedText => onResponse: Receivied Text : \n"+ generatedText[0].trim());
+                    } catch (JSONException e) {
+                        Log.d(TAG2, "onResponse: "+e.getMessage());
+                    }
+                }
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+    }
+
+    private void createErrorSnackBar(String msg, boolean isError) {
         Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content), msg, Snackbar.LENGTH_LONG);
         snackbar.setBackgroundTint(ContextCompat.getColor(this, R.color.green_500));
-        if (isError){
+        if (isError) {
             snackbar.setBackgroundTint(ContextCompat.getColor(this, R.color.red_500));
         }
         snackbar.setTextColor(ContextCompat.getColor(this, R.color.white_900));
         snackbar.show();
 
+    }
+
+    // Interface
+
+    public interface AICompletionCallback {
+        void onCompletion(String generatedText);
     }
 }
